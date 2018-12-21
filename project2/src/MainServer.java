3cba22dbbadf5f75a1b3f2c7791a084a38fbe4f4
irfanm96 <e15138@ce.pdn.ac.lk>
2018-12-21 15:34:59 +0530

    added comments to mainserver

--- project2/src/MainServer.java
+++ project2/src/MainServer.java
@@ -6,46 +6,32 @@ import java.net.Socket;
 public class MainServer { 
 
     /* Some constants */     
-    public static final int BASE_PORT = 1250;  // do not change    
+    public static final int BASE_PORT = 2000;  // port number for listening
 
 
     /* local data for the server 
-     * Every main server is defined in terms of the port it 
-     * listens and the database of allowed initailStocks 
+     * Every main server is defined in terms of the port
      */ 
     private ServerSocket serverSocket=null;  // server Socket for main server 
-    private StockDB initialStocks;     // who are allowed to chat
+    private StockDB initialStocks;     // initial stock values
 
     public MainServer(int socket, StockDB initialStocks) {
 	this.initialStocks = initialStocks;
 
 	try { 
-	    this.serverSocket = new ServerSocket(socket); 
+	    this.serverSocket = new ServerSocket(socket); //create new serverSocket
 	} catch (IOException e) { 
 	    System.out.println(e); 
 	}
     }
 
-    /* each server will provide the following functions to 
-     * the public. Note that these are non-static 
-     */ 
-
-
-    /* server will define how the messages should be posted 
-     * this will be used by the connection servers
-     */ 
-
-    public void postMSG(String msg) { 
-	// all threads print to same screen 
-	System.out.println(msg); 
-    }
-
+    //server listening for requests and create threads for each connection
     public void server_loop() { 
 	try { 
 	    while(true) { 
 		Socket socket = this.serverSocket.accept(); 
 		ConnectionServer worker = new ConnectionServer(this); 
-		worker.handleConnection(socket); 
+		worker.handleConnection(socket); //handle the socket
 	    }
 	} catch(IOException e) { 
 	    System.out.println(e);
